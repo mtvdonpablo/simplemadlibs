f2ec1247b2a84191c00d29a93b9becbff364d42f
mtvdonpablo <donjoseph28@gmail.com>
2022-09-16 09:58:08 -0400

    first commit

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#string concatenation 
+youtuber ="Don Joseph"
+##print("sub to {}".format(youtuber))
+#print(f"sub to {youtuber}") #f string
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+pronoun = input("Pronoun: ")
+madlib= f"{famous_person} is {adj}! {pronoun} are amazing at {verb1} and {verb2}."
+
+print(madlib)
